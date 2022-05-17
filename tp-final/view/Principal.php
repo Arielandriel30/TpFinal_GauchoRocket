f97f83418bb18835f7727d594058e219f2e0f334
arielandriel <46574963+arielandriel@users.noreply.github.com>
2022-05-17 19:28:06 -0300

    btn de busqueda

--- tp-final/view/Principal.php
+++ tp-final/view/Principal.php
@@ -3,13 +3,25 @@
 
   <div class="w3-center">
   <h4 class="texto1">GAUCHO ROCKET</h4>
-  <h1 class=" w3-animate-bottom texto1" >LOS MEJORES VIAJES ESPACIALES</h1>
+  <h1 class=" w3-animate-bottom  texto1" >LOS MEJORES VIAJES ESPACIALES</h1>
     <div class="w3-padding-32">
-    <a href='index.php?page=login' target="_parent"><button class="w3-btn w3-xlarge w3-dark-grey parpadeo w3-round-large w3-hover-light-grey" onclick="document.getElementById('id01').style.display='block'" style="font-weight:900;">INGRESAR</button></a>
-    <a href='index.php?page=registro' target="_parent"><button class="w3-btn w3-xlarge w3-dark-grey parpadeo w3-round-large w3-hover-light-grey" onclick="document.getElementById('id01').style.display='block'" style="font-weight:900;">REGISTRARSE</button></a>
+    <a href='index.php?page=login' target="_parent"><button class="w3-btn w3-xlarge w3-pink  parpadeo w3-round-large w3-hover-light-grey"  style="font-weight:900;">INGRESAR</button></a>
+    <a href='index.php?page=registro' target="_parent"><button class="w3-btn w3-xlarge  w3-food-spearmint parpadeo w3-round-large w3-hover-light-grey"  style="font-weight:900;">REGISTRARSE</button></a>
     </div>
   </div>
-</header>
+  </header>
+  <div class="w3-row-padding">
+<form action="" class="desplazar2 w3-margin">
+  
+  <div class="w3-third w3-margin">
+    <input class="w3-input w3-border" type="text"  placeholder="Busca tu próximo viaje">
+  </div>
+  <div class="w3-third w3-margin">
+    <input class="w3-btn w3-large w3-food-spearmint w3-round-large  w3-hover-pink" style="width:100px; font-weight:900;" type="submit" placeholder="Buscar">
+  </div>
+ </form>
+</div>
+
 <div class="w3-row-padding w3-center w3-margin-top">
 <div class="w3-third">
   <div class="w3-card w3-container" style="min-height:460px">
--- tp-final/view/header.php
+++ tp-final/view/header.php
@@ -8,8 +8,10 @@
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-black.css">
 <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="stylesheet" href="https://www.w3schools.com/lib/w3-colors-food.css">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Tomorrow&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://www.w3schools.com/lib/w3-colors-vivid.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="assets/style.css">
 
