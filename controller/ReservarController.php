3b1825bc12f19d2fbab870ea8e35a982facc3eb3
FernandoSullca <42738690+FernandoSullca@users.noreply.github.com>
2022-06-19 19:44:02 -0300

    agregando metodos

--- controller/ReservarController.php
+++ controller/ReservarController.php
@@ -16,9 +16,41 @@ class ReservarController
         $this->ReservaModel = $ReservaModel;
     }
 
+    public  function validate(){
+     $nivelMedico= $this->ReservaModel->getNivelMedico();
+     if(!$nivelMedico){
+
+         $this->generalTurno();
+         $this->RealizarChequeo();
+     }
+     else{
+         $this->ReservaModel->getNivelMedico();
+         $this->validarNivelMedico();
+     }
+            $this->RealizarReserva();
+
+
+    }
+
     public function execute() {
 
         $this->printer->generateView('Reserva.html');
+    }
+
+    private function generalTurno()
+    {
+    }
+
+    private function validarNivelMedico()
+    {
+    }
+
+    private function RealizarChequeo()
+    {
+    }
+
+    private function RealizarReserva()
+    {
     }
 
 }
