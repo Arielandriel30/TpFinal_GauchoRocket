725311f20439e577fbd1ebc4566b6732113475d0
1cristianb <crboschi@alumno.unlam.edu.ar>
2022-06-24 22:30:18 -0300

    envio de datos de reserva a compra

--- controller/CompraController.php
+++ controller/CompraController.php
@@ -24,14 +24,31 @@ class CompraController
             $user = $this->session->sessionShow('usuario');
             $data = array("user"=>$user);
         $this->printer->generateView('Compra.html', $user);
-     }
+     //}
     }
     public function confirmarCompra() {
-
-        $this->printer->generateView('Confirmacion.html');
+        if(isset($_POST['submit'])){
+            if(!empty($_POST['total']))
+            $total=$_POST['total'];
+        }
+        /*aca iria un model para convertir creditos a moneda*/
+        $dineroLocal=$total*2.59;
+        $data = array("valor"=>$total,"dinero"=>$dineroLocal);
+        $this->printer->generateView('Confirmacion.html',$data);
     }
     public function generarQr() {
         $this->qr->crearQr('Recibo de pago $ 8500 vuelo gaucho rcoket SA.');
     }
 
+    public function mostrarVuelosReservados(){
+        if(isset($_POST['submit'])){
+            if(!empty($_POST['vuelos'])){
+                $vuelos=$_POST['vuelos'];
+
+            }
+        }
+        $valorTotal=sizeof($vuelos)*1000;
+        $data = array("vuelo"=>$vuelos,"valor"=>1000,"total"=>$valorTotal);
+        $this->printer->generateView('Compra.html',$data);
+    }
 }
